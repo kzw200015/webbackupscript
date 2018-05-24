cbc0e9d207a457a8ef1364fd6f50c2217e4cec9d
kzw200015 <kzw200015@gmail.com>
2018-05-24 23:42:02 +0800

    添加自动删除三天前的备份

--- autobackup.sh
+++ autobackup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 backup_date=$(date +"%Y%m%d")
+old_backup_date=$(date -d -3day +"%Y%m%d")
 # webdav用户名
 username=""
 # webdav密码
@@ -13,8 +14,10 @@ rm -rf /home/backup/* &&
 echo "backuping" &&
 # 下面是lnmp一键包的备份脚本的路径
 /home/kzw/lnmp*/tools/backup.sh &&
-echo "packaging" &&
+echo "packaging" &&6
 tar -zcf /home/backup-${backup_date}.tar.gz -C /home backup &&
 echo "uploading" &&
 curl -u "${username}:${password}" -T "/home/backup-${backup_date}.tar.gz" "${server_address}" &&
-rm -f /home/backup-${backup_date}.tar.gz
+rm -f /home/backup-${backup_date}.tar.gz &&
+echo "deleting old backups on the remote server"
+curl -u "${username}:${password}" -X DELETE "${server_address}backup-${old_backup_date}.tar.gz" 
